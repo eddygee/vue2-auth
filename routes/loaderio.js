989504d65e3e08a3c0e34c15ca079bcc3ef5adee
Edgar Garcia <egarcia607@gmail.com>
2015-03-31 11:00:52 -0400

    restucture app

--- routes/loaderio.js
+++ routes/loaderio.js
@@ -4,10 +4,15 @@ var BSON = require('mongodb').BSONPure;
 
 /* GET spots listing. */
 router.get('/', function(req, res, next) {
-  
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  var str = 'loaderio-1c8e8b375aa7f7ac9c319a7a42f2182e';
-  //console.log(rtn);
-  res.send(str);
-
+  //res.send('respond with a resource');
+  var db = req.db;
+  db.collection('spots').find({status:'published'}).toArray(function (err, items) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.type('application/json');
+    var str = 'loaderio-1c8e8b375aa7f7ac9c319a7a42f2182e';
+    var rtn  = JSON.stringify(items);
+    //console.log(rtn);
+    res.send(rtn);
+    //res.json(items);
+  });
 });
